66d15b540d1477a1fa91b539ee369621165d35a6
Dmitrii <ezikvice@mail.ru>
2018-06-29 11:33:20 +0300

    First commit. questions.csv, domain classes for questions and answers

---
+++ homework1/src/main/java/ru/ezikvice/springotus/Main.java
@@ -0,0 +1,11 @@
+package ru.ezikvice.springotus;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+
+    }
+}
---
+++ homework1/src/main/java/ru/ezikvice/springotus/domain/Question.java
@@ -0,0 +1,33 @@
+package ru.ezikvice.springotus.domain;
+
+import java.util.Set;
+
+public class Question {
+    private int id;
+    private String text;
+    private Set<Answer> answers;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Set<Answer> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(Set<Answer> answers) {
+        this.answers = answers;
+    }
+}
